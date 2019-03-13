dbdb9f4fa1414445e4c3889d7955c0f6570d87c7
ToAnh Tran <toanh.t.tran@gmail.com>
2019-03-13 20:08:23 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,25 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+  constructor(prop){
+    super();
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    handleChange(event){
+      this.setState({
+        input:event.target.value
+      });
+    }
+      render(){
+        return(
+          <button onCick={this.state.} />
+        )
+      }
+    }
+  }
+}
+
+export default CoordinatesButton;
